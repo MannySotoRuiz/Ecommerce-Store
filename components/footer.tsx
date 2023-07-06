1b11dd252d37cb02e3c1aa3a3ee87db36ea8fec0
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-07-06 17:10:40 -0400

    created temp simple footer

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 EcommerceStore, Inc. All Rights reserved | Website
+          developed by Emmanuel Soto Ruiz
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
