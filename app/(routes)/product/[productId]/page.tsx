1b50990505a9b94213a39d4d001de002fe194618
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-07-07 17:07:24 -0400

    created gallery for individual product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/ui/container";
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
+import Gallery from "@/components/gallery";
 
 export const revalidate = 0;
 
@@ -22,8 +23,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            {/* gallery */}
-            <div>Gallery</div>
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               {/* info */}
               Info
