a7c9a540c74f0757d997db6f247b005d3ee0d8b8
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-07-07 15:47:03 -0400

    created billboard component to display billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,13 @@
-const HomePage = () => {
-  return <div></div>;
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">{/* billboard */}</div>
+    </Container>
+  );
 };
 
 export default HomePage;
