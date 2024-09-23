71926328bb80d3b8679d4b81f38db01183d19599
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-09-22 23:23:39 -0400

    updated billboard id

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,17 +8,14 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("b192dd76-1cfc-4fa7-b25e-f81ea48f8b2d");
+  const billboard = await getBillboard("a6ae1266-6643-4aac-a53d-ccb888c106a1");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList
-            title="Featured Products"
-            items={products}
-          />
+          <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
